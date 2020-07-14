814f1b455db732834a914b3bd100c024b653c4a9
tatsuya_haba@densan-s.co.jp <tatsuya_haba@densan-s.co.jp>
2020-07-14 17:42:44 +0900

    changed httpd.conf for routing in fuelphp

--- app/fuel/app/config/routes.php
+++ app/fuel/app/config/routes.php
@@ -2,6 +2,6 @@
 return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
-	
+	'hoge' => 'welcome/hello',
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
